fafaf3a0fdf0784c0b5557efddaaeff9ffd54756
Ploshka <folga12381@gmail.com>
2022-12-03 20:32:36 +1000

    Receiving id changed

--- src/Entity/Receiving.php
+++ src/Entity/Receiving.php
@@ -9,15 +9,11 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: ReceivingRepository::class)]
 class Receiving
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
-
     #[ORM\ManyToOne(inversedBy: 'receivings')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Worker $worker = null;
 
+    #[ORM\Id]
     #[ORM\OneToOne(inversedBy: 'receiving', cascade: ['persist', 'remove'])]
     #[ORM\JoinColumn(nullable: false)]
     private ?WorkClothing $workClothing = null;
@@ -27,11 +23,6 @@ class Receiving
 
     #[ORM\Column(length: 255)]
     private ?string $signature = null;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
 
     public function getWorker(): ?Worker
     {
