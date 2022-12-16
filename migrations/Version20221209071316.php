84c94fb1525287a9ee55938f2ae406b4adc7b452
Ploshka <folga12381@gmail.com>
2022-12-16 17:21:16 +1000

    Migration errors fixed

--- migrations/Version20221209071316.php
+++ migrations/Version20221209071316.php
@@ -29,9 +29,9 @@ final class Version20221209071316 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE department CHANGE name name VARCHAR(255) NOT NULL, CHANGE chief_name chief_name VARCHAR(255) NOT NULL, DROP CONSTRAINT CHK_DepStrLength');
-        $this->addSql('ALTER TABLE worker CHANGE name name VARCHAR(255) NOT NULL, DROP CONSTRAINT CHK_WorkerStrLength');
-        $this->addSql('ALTER TABLE work_clothing CHANGE type type VARCHAR(255) NOT NULL, DROP CONSTRAINT CHK_WorkClothStrLength');
-        $this->addSql('ALTER TABLE post CHANGE name name VARCHAR(255) NOT NULL, DROP CONSTRAINT CHK_PostStrLength');
+        $this->addSql('ALTER TABLE department CHANGE name name VARCHAR(255) NOT NULL, CHANGE chief_name chief_name VARCHAR(255) NOT NULL, DROP CHECK CHK_DepStrLength');
+        $this->addSql('ALTER TABLE worker CHANGE name name VARCHAR(255) NOT NULL, DROP CHECK CHK_WorkerStrLength');
+        $this->addSql('ALTER TABLE work_clothing CHANGE type type VARCHAR(255) NOT NULL, DROP CHECK CHK_WorkClothStrLength');
+        $this->addSql('ALTER TABLE post CHANGE name name VARCHAR(255) NOT NULL, DROP CHECK CHK_PostStrLength');
     }
 }
