cc312b28a9450aabb2127a5930813d4d576f1a14
Ploshka <folga12381@gmail.com>
2022-12-24 13:12:46 +1000

    Styles changed

--- assets/vue/controllers/Table.vue
+++ assets/vue/controllers/Table.vue
@@ -2,6 +2,11 @@
     <ContextMenu ref="ctxmenu" @option-clicked="option"/>
     <table oncontextmenu="return false;">
         <tr>
+            <td id="button-container">
+                <button id="send-button" :class="buttonClass">Отправить</button>
+            </td>
+        </tr>
+        <tr>
             <th v-for="header in headers">{{ header }}</th>
         </tr>
         <TableRow v-for="row in table" :item="row" :ctxm="ctxmenu" @menu="ctx"/>
@@ -16,9 +21,11 @@
     defineProps({
         headers: Array,
         table: Array,
+        link: String,
     });
 
     const ctxmenu = ref(null)
+    const buttonClass = ref('')
 
     function ctx(item, event, gg)
     {
@@ -53,11 +60,36 @@
             text-align: left;
             padding: 3px;
         }
+        #button-container
+        {
+            border: none;
+        }
 
         th
         {
             @extend td;
             font-weight: bold;
+        }
+    }
+    #send-button
+    {
+        border: none;
+        background-color: rgb(173, 173, 173);
+        color: rgb(0, 0, 0);
+        text-decoration: none;
+        text-align: left;
+        padding: 5px;
+        border-radius: 0;
+        width: 50%;
+        height: 100%;
+        min-width: 100px;
+        &.active
+        {
+            background-color: rgb(0, 255, 0);
+            &:hover
+            {
+                background-color: rgb(0, 205, 0);
+            }
         }
     }
 </style>
